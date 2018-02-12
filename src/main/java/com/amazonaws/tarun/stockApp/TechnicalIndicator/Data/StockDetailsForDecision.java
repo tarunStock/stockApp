d0bf86479b4f18b691ef084b23491a038c41e87f
tarunstock <tarunstockcomm@gmail.com>
2018-02-12 14:42:25 +0000

    Checking

--- src/main/java/com/amazonaws/tarun/stockApp/TechnicalIndicator/Data/StockDetailsForDecision.java
+++ src/main/java/com/amazonaws/tarun/stockApp/TechnicalIndicator/Data/StockDetailsForDecision.java
@@ -5,13 +5,14 @@ import java.time.LocalDate;
 public class StockDetailsForDecision {
 	public String stockCode;
 	public LocalDate suggestedDate; 
-	public String BBTrend;
+	
 	public float ChandelierExit;
 	public float CurrentPrice;
 	public long CurrentVolume;
 	public float OneDayPreviousPrice;
 	public long OneDayPreviousVolume;
 	public float RSIValue;
+	public String BBTrend;
 	public float ThreeDayPreviousPrice;
 	public float TwoDayPreviousPrice;
 	public long TwoDayPreviousVolume;

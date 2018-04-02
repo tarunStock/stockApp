071b6709e77831fbdeca31be234d8ff5680f51b8
tarunstock <tarunstockcomm@gmail.com>
2018-04-02 02:09:09 +0000

    data object for new Approach of suggestions

---
+++ src/main/java/com/amazonaws/tarun/stockApp/TechnicalIndicator/Data/StockDataForNewApproach.java
@@ -0,0 +1,10 @@
+package com.amazonaws.tarun.stockApp.TechnicalIndicator.Data;
+
+public class StockDataForNewApproach extends StockDetailsForDecision{
+	public boolean MACDIncreasing;
+	public boolean smallerSMAIncreasing;
+	public boolean middleSMAIncreasing;
+	public boolean accumulationDistributionIncreasing;
+	public boolean stochasticInLine;
+	public boolean OBVIncreasing;
+}
